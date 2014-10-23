80aaed2a3ba7af79eebcef6b5a19897442f88b09
PengJiEmory <peng.ji@emory.edu>
2014-10-23 17:13:47 -0400

    html demo for author and reviewer

--- Design/Mockup/html demo/js/app.js
+++ Design/Mockup/html demo/js/app.js
@@ -4,3 +4,17 @@ $(document).ready(function() {
         placement : 'top'
     });
 });
+$( document.body ).on( 'click', '.dropdown-menu li', function( event ) {
+  var $target = $( event.currentTarget );
+  $target.closest( '.btn-group' )
+     .find( '[data-bind="label"]' ).text( $target.text() )
+        .end()
+     .children( '.dropdown-toggle' ).dropdown( 'toggle' );
+  return false;
+});
+
+/* assigned by class */
+$(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); } );
+
+/* assigned by element */
+$(function(){$(".validated").jqBootstrapValidation();});
