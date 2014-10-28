add1ac8898609fb3edad36e37150ee14ed5f876c
Johnny Tan <jtan25@emory.edu>
2014-10-28 10:35:33 -0400

    changed reviewer home adding test cases

--- PeerReview/PeerReviewApp/tests.py
+++ PeerReview/PeerReviewApp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from models import *
+from django.utils import timezone
+from forms import *
+from django.test import Client
 
 # Create your tests here.
+
+
+class ReviewPeriodTest(TestCase):
+    def setUp(self):
+
+
+
+
+    def test_invalid_review_period(self):
