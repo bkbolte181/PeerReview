5b1e12d19218aabe9ab9c4d2c8e8608b889474d7
Johnny Tan <jtan25@emory.edu>
2014-10-28 13:05:03 -0400

    Added usertestcase

--- PeerReview/PeerReviewApp/tests.py
+++ PeerReview/PeerReviewApp/tests.py
@@ -3,7 +3,21 @@ from models import *
 from django.utils import timezone
 from forms import *
 from django.test import Client
+from django.utils import unittest
+from django.core.urlresolvers import reverse
+from forms import *
+
 
 # Create your tests here.
 
+class UserTest(TestCase):
+
+    def create_user(self, email = 'johnny@emory.edu', password = '123', firstname = 'johnny', lastname= 'tan'):
+        return SiteUser.objects.create(email = email, password = password, first_name = firstname, last_name = lastname)
+
+    def test_user_creation(self):
+        w = self.create_user()
+        self.assertTrue(isinstance(w,SiteUser))
+        self.assertEqual(w.get_short_name(), 'johnny')
+        self.assertEqual(w.get_full_name(),('johnny','tan'))
 
