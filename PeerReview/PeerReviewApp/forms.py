543bced9c80f641bae3590981d55f5b236d86fb7
Johnny Tan <jtan25@emory.edu>
2014-10-30 02:04:55 -0400

    Added manuscript review count

--- PeerReview/PeerReviewApp/forms.py
+++ PeerReview/PeerReviewApp/forms.py
@@ -52,7 +52,7 @@ class SignupForm(forms.ModelForm):
 	class Meta:
 		model = SiteUser
 		# These are the fields that the user needs to input when they create their account
-		fields = ('email', 'first_name', 'last_name', 'department', 'lab', 'pi', 'school')
+		fields = ('email', 'first_name', 'last_name', 'department', 'lab', 'pi', 'school','Review_Count')
 		widgets = {
 			'email': forms.EmailInput(attrs={'class': 'form-control text-center', 'placeholder': 'Emory Email Address'}),
 			'first_name': forms.TextInput(attrs={'class': 'form-control text-center', 'placeholder': 'First Name'}),
@@ -61,6 +61,7 @@ class SignupForm(forms.ModelForm):
 			'lab': forms.TextInput(attrs={'class': 'form-control text-center', 'placeholder': 'Name of Lab'}),
 			'pi': forms.TextInput(attrs={'class': 'form-control text-center', 'placeholder': 'Name of Primary Investigator'}),
 			'school': forms.Select(attrs={'class': 'form-control text-center', 'placeholder': 'Choose your School'}),
+            'Review_Count': forms.TextInput(attrs={'class': 'form-control text-center','placeholder': 'Number of manuscripts previous reviewed'}),
 		}
 	
 	# This method validates that the two passwords are the same
