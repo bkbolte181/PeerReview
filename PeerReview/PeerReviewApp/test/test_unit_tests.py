52ec2d31f97bb0d9709cc4ef3328ffd73d37c392
hjl590590@gmail.com <hjl590590@gmail.com>
2014-12-08 13:02:26 -0800

    unit tests final

--- PeerReview/PeerReviewApp/test/test_unit_tests.py
+++ PeerReview/PeerReviewApp/test/test_unit_tests.py
@@ -1,12 +1,14 @@
-from django.test import TestCase
-from django.utils import timezone
-from django.test import Client
 from django.utils import unittest
+from django.utils import timezone
+from django.test import TestCase
+from django.test import Client
 from django.core.urlresolvers import reverse
-import datetime
+
 from PeerReviewApp.models import *
 from PeerReviewApp.forms import *
 from PeerReviewApp.views import *
+
+import datetime
 
 #PeerReviewApp.test.test_unit_tests
 #test create user
@@ -35,6 +37,7 @@ class GetFullNameTest(unittest.TestCase):
 
 #test get_star_string
 class GetStarStringTest(unittest.TestCase):
+
 	def test_get_star_string_true(self):
 		instance = SiteUser(email = 'jiulin@emory.edu', password = '123'
 		, review_count = 3)
@@ -45,6 +48,63 @@ class GetStarStringTest(unittest.TestCase):
 		, review_count = 2)
 		self.assertEqual(instance._get_star_string(), (''))
 
+#test is_normal_user_ 
+class IsNormalUserTest(unittest.TestCase):
+	
+	def test_is_normal_user_true(self):
+		instance = SiteUser(email = 'jiulin@emory.edu', password = '123'
+		, review_count = 3, is_site_admin = False)
+		self.assertEqual(is_normal_user_(instance), True)
+
+	def test_is_normal_user_false(self):
+		instance = SiteUser(email = 'jiulin@emory.edu', password = '123'
+		, review_count = 3, is_site_admin = True)
+		self.assertEqual(is_normal_user_(instance), False)
+
+#test is_normal_user_check
+class IsAuthenticatedNormalUserTest(unittest.TestCase):
+	
+	def test_is_normal_user_check_true(self):
+		instance = SiteUser(email = 'jiulin@emory.edu', password = '123'
+		, review_count = 3, is_site_admin = False)
+		self.assertEqual(is_normal_user_check(instance), True)
+
+	def test_is_normal_user_check_false(self):
+		instance = SiteUser(email = 'jiulin@emory.edu', password = '123'
+		, review_count = 3, is_site_admin = True)
+		self.assertEqual(is_normal_user_check(instance), False)
+
+#test is_site_admin_check
+class IsAuthenticatedAdminTest(unittest.TestCase):
+	
+	def test_is_site_admin_check_true(self):
+		instance = SiteUser(email = 'jiulin@emory.edu', password = '123'
+		, review_count = 3, is_site_admin = True)
+		self.assertEqual(is_site_admin_check(instance), True)
+
+	def test_is_site_admin_check_false(self):
+		instance = SiteUser(email = 'jiulin@emory.edu', password = '123'
+		, review_count = 3, is_site_admin = False)
+		self.assertEqual(is_site_admin_check(instance), False)
+
+#test has_agreed
+class HasAgreedTest(unittest.TestCase):
+
+	def test_has_agreed_admin(self):
+		instance = SiteUser(email = 'jiulin@emory.edu', password = '123'
+		, review_count = 3, is_site_admin = True)
+		self.assertEqual(has_agreed(instance), False)
+
+	def test_has_agreed_true(self):
+		instance = SiteUser(email = 'jiulin@emory.edu', password = '123'
+		, review_count = 3, is_site_admin = False, agreed_to_form = True)
+		self.assertEqual(is_normal_user_check(instance), True)
+
+	def test_has_agreed_false(self):
+		instance = SiteUser(email = 'jiulin@emory.edu', password = '123'
+		, review_count = 3, is_site_admin = True, agreed_to_form = False)
+		self.assertEqual(is_normal_user_check(instance), False)
+
 #test create review period
 class CreateReviewPeriodTest(unittest.TestCase):
 
@@ -54,6 +114,16 @@ class CreateReviewPeriodTest(unittest.TestCase):
 		, review_deadline = timezone.now()+datetime.timedelta(days = 30)
 		, group_meeting_time = timezone.now()+datetime.timedelta(days = 50))
 		self.assertTrue(isinstance(instance, ReviewPeriod))
+
+#test get current review period
+#class GetCurrentReviewPeriodTest(unittest.TestCase):
+
+#	def test_get_current_review_period():
+#		instance = ReviewPeriod(is_full = 'False', start_date = timezone.now()
+#		, submission_deadline = timezone.now()
+#		, review_deadline = timezone.now()+datetime.timedelta(days = 30)
+#		, group_meeting_time = timezone.now()+datetime.timedelta(days = 50)
+#		, is_current = True)		
 
 #test create manuscript 
 class CreateManuscriptTest(unittest.TestCase):
